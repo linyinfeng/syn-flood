812a8c4345be04a1fdfad39760608e05245ce871
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-30 00:49:16 +0000

    Deploy linyinfeng/syn-flood to github.com/linyinfeng/syn-flood.git:gh-pages

--- implementors/structopt/trait.StructOpt.js
+++ implementors/structopt/trait.StructOpt.js
@@ -1,4 +1,5 @@
 (function() {var implementors = {};
+implementors["structopt"] = [];
 implementors["syn_flood"] = [{"text":"impl <a class=\"trait\" href=\"structopt/trait.StructOpt.html\" title=\"trait structopt::StructOpt\">StructOpt</a> for <a class=\"struct\" href=\"syn_flood/option/struct.Opt.html\" title=\"struct syn_flood::option::Opt\">Opt</a>","synthetic":false,"types":["syn_flood::option::Opt"]}];
 
             if (window.register_implementors) {
---
+++ implementors/structopt/trait.StructOptInternal.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["syn_flood"] = [{"text":"impl StructOptInternal for <a class=\"struct\" href=\"syn_flood/option/struct.Opt.html\" title=\"struct syn_flood::option::Opt\">Opt</a>","synthetic":false,"types":["syn_flood::option::Opt"]}];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
