4bfa14d60c508e18a83c20304f0856e327be5726
linyinfeng <linyinfeng@users.noreply.github.com>
2021-04-25 07:00:57 +0000

    deploy: 6459191c525d6b10fcf117353a48d9190ab1e38f

--- implementors/structopt/trait.StructOptInternal.js
+++ implementors/structopt/trait.StructOptInternal.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["syn_flood"] = [{"text":"impl StructOptInternal for Opt","synthetic":false,"types":[]}];
+implementors["syn_flood"] = [{"text":"impl <a class=\"trait\" href=\"structopt/trait.StructOptInternal.html\" title=\"trait structopt::StructOptInternal\">StructOptInternal</a> for <a class=\"struct\" href=\"syn_flood/option/struct.Opt.html\" title=\"struct syn_flood::option::Opt\">Opt</a>","synthetic":false,"types":["syn_flood::option::Opt"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- pnet_packet/icmp/echo_request/IcmpCodes/sidebar-items.js
+++ pnet_packet/icmp/echo_request/IcmpCodes/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["NoCode","0 is the only available ICMP code for \"echo reply\" ICMP packets."]]});
+initSidebarItems({"constant":[["NoCode","0 is the only available ICMP code for “echo reply” ICMP packets."]]});
